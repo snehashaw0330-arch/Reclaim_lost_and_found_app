b5bc9b5f0743560cc5aaa13912c063d919bac017
Sneha Shaw <snehashaw0330@gamil.com>
2026-01-20 23:43:09 +0530

    6th commit

---
+++ backend/convert_to_embed.py
@@ -0,0 +1,19 @@
+from sentence_transformers import SentenceTransformer
+from PIL import Image
+from typing import List
+
+
+class ImageEmbedder:
+    def _init_(self):
+        # CLIP model for image embeddings
+        self.model = SentenceTransformer("clip-ViT-B-32")
+
+    def image_to_embedding(self, image_path: str) -> List[float]:
+        """
+        Converts an image to a vector embedding.
+        """
+        image = Image.open(image_path).convert("RGB")
+        embedding = self.model.encode(image)
+
+        # Convert numpy array to Python list
+        return embedding.tolist()
